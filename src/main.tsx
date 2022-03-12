6b16bf0c0b22b4c51124fd5c67ae6a4c52ae8e03
Vu Tien Long <tllong20002@gmail.com>
2022-03-12 18:11:54 +0700

    react buoi 1

---
+++ src/main.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+
+
+const myName:string = "Vũ Tiến Long";
+const age:number = 20;
+const phone:string = "0392127565"
+const arr: {name:string, age:number} = {name:"long", age: 20}
+
+type ShowProps = {
+  name: string
+}
+
+function show(props:ShowProps):any {
+  console.log(props.name);
+  
+}
+function Show(props:ShowProps):any {
+  console.log('props',props);
+  console.log(props.name);
+  return null
+  
+}
+ReactDOM.render(
+  <div><h1>Hello {myName}</h1> 
+      <div>Age: {age}</div>
+      <div>Arr: {arr.name}</div>
+      <div>{show({name:"myName"})}</div>
+      <div><Show name={myName} /></div>
+  </div>, document.querySelector('#root')
+)
